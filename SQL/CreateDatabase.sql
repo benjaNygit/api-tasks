febf2cb8100061a4700d59d4dcda199cef51098e
Benjamin Maldonado <benja.ignacio2001@gmail.com>
2024-03-02 17:51:58 -0300

    Script creación de tablas

--- SQL/CreateDatabase.sql
+++ SQL/CreateDatabase.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE Tasks_DB
+-- CREATE DATABASE Tasks_DB
+
 /**
 DROP DATABASE Tasks_DB
 
@@ -10,71 +11,37 @@ DROP TABLE dbo.Task
 
 -- Crear Tabla Category
 CREATE TABLE "Category" (
-	"Code"	INTEGER,
-	"Description"	INTEGER NOT NULL UNIQUE,
+	"Code" INTEGER,
+	"Description" TEXT NOT NULL UNIQUE,
 	PRIMARY KEY("Code" AUTOINCREMENT)
 );
 
--- Crear Tabla Priority
-CREATE TABLE [dbo].[Priority](
-	[Code] [tinyint] NOT NULL,
-	[Description] [nvarchar](25) NOT NULL,
- CONSTRAINT [PK_Priority] PRIMARY KEY CLUSTERED 
-(
-	[Code] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+CREATE TABLE "Priority" (
+	"Code" INTEGER,
+	"Description" TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("Code" AUTOINCREMENT)
+);
 
--- Crear Tabla User
-CREATE TABLE [dbo].[User](
-	[Id] [uniqueidentifier] NOT NULL,
-	[Name] [nvarchar](30) NOT NULL,
-	[Email] [nvarchar](100) NOT NULL,
-	[Password] [nvarchar](32) NOT NULL,
-	[CreateAt] [datetime] NOT NULL,
-	[LastSession] [datetime] NOT NULL,
- CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+CREATE TABLE "User" (
+	"Id"	TEXT,
+	"Name"	TEXT UNIQUE,
+	"Email"	TEXT NOT NULL UNIQUE,
+	"Password"	TEXT NOT NULL,
+	"CreatedAt"	TEXT NOT NULL,
+	"LastSession"	TEXT NOT NULL,
+	PRIMARY KEY("Id")
+);
 
--- Crear Tabla Task
-CREATE TABLE [dbo].[Task](
-	[Id] [uniqueidentifier] NOT NULL,
-	[Title] [nvarchar](50) NOT NULL,
-	[Description] [nvarchar](max) NOT NULL,
-	[CategoryCode] [tinyint] NOT NULL,
-	[PriorityCode] [tinyint] NOT NULL,
-	[CreateAt] [datetime] NOT NULL,
-	[Done] [bit] NOT NULL,
-	[UserId] [uniqueidentifier] NOT NULL,
- CONSTRAINT [PK_Task] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Task]  WITH CHECK ADD  CONSTRAINT [FK_Task_Category] FOREIGN KEY([CategoryCode])
-REFERENCES [dbo].[Category] ([Code])
-GO
-
-ALTER TABLE [dbo].[Task] CHECK CONSTRAINT [FK_Task_Category]
-GO
-
-ALTER TABLE [dbo].[Task]  WITH CHECK ADD  CONSTRAINT [FK_Task_Priority] FOREIGN KEY([PriorityCode])
-REFERENCES [dbo].[Priority] ([Code])
-GO
-
-ALTER TABLE [dbo].[Task] CHECK CONSTRAINT [FK_Task_Priority]
-GO
-
-ALTER TABLE [dbo].[Task]  WITH CHECK ADD  CONSTRAINT [FK_Task_User] FOREIGN KEY([UserId])
-REFERENCES [dbo].[User] ([Id])
-GO
-
-ALTER TABLE [dbo].[Task] CHECK CONSTRAINT [FK_Task_User]
-GO
+CREATE TABLE "Task" (
+	"Id"	TEXT,
+	"Title"	TEXT NOT NULL,
+	"Description"	TEXT NOT NULL,
+	"CategoryCode"	INTEGER NOT NULL,
+	"PriorityCode"	INTEGER NOT NULL,
+	"CreateAt"	TEXT NOT NULL,
+	"Done"	INTEGER NOT NULL DEFAULT 1,
+	"UserId"	TEXT NOT NULL,
+	FOREIGN KEY("CategoryCode") REFERENCES "Category"("Code"),
+	FOREIGN KEY("PriorityCode") REFERENCES "Priority"("Code"),
+	PRIMARY KEY("Id")
+);
