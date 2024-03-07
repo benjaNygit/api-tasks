e7936de346c703d7a5db4bb9c38e0ec67454da83
Benjamin Maldonado <benja.ignacio2001@gmail.com>
2024-03-06 21:42:18 -0300

    Create Delete Category

--- models/Category.go
+++ models/Category.go
@@ -1,12 +1,29 @@
 package models
 
+import (
+	"log"
+
+	"github.com/benjaNygit/api-tasks/db"
+)
+
 type Category struct {
 	Code        uint   `json:"code"`
 	Description string `json:"description"`
 }
 
 func (c *Category) Create() bool {
-	return false
+	db.ConnectDB()
+	defer db.DB.Close()
+
+	query := db.Insert("Category", c.Description)
+
+	_, err := db.DB.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+
+	return true
 }
 
 func (c *Category) Read() Category {
@@ -18,5 +35,16 @@ func (c *Category) Update() Category {
 }
 
 func (c *Category) Delete() bool {
-	return false
+	db.ConnectDB()
+	defer db.DB.Close()
+
+	query := db.Delete("Category", string(rune(c.Code)))
+
+	_, err := db.DB.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+
+	return true
 }
